806df357274983b3ae38703e8a88cb50ebe94642
Ryan Bethel <ryan.bethel@gmail.com>
2021-04-30 06:40:04 -0400

    added short hash to app name

--- .github/workflows/smoke-tests/staging/debug-test.js
+++ .github/workflows/smoke-tests/staging/debug-test.js
@@ -1,9 +1,10 @@
 const tiny = require("tiny-json-http");
 const test = require("tape");
 
-const baseUrl = "http://localhost:3333";
+const baseUrl = process.env.STAGING_URL;
 
 test("Sanity Test Staging", async (t) => {
     t.plan(1);
+    console.log(baseUrl);
     t.ok(true, "Sanity Test Staging");
 });
